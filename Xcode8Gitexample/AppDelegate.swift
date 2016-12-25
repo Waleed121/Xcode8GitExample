b742fc9a1f09bd71efb7aa76f3761f34f3df6737
Waleed Siddiqui <wsiddiqui@Waleeds-MacBook-Pro.local>
2016-12-24 22:38:38 -0500

    Initial Commit

---
+++ Xcode8Gitexample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8Gitexample
+//
+//  Created by Waleed Siddiqui on 2016-12-24.
+//  Copyright © 2016 Waleed Siddiqui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8Gitexample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8Gitexample
+//
+//  Created by Waleed Siddiqui on 2016-12-24.
+//  Copyright © 2016 Waleed Siddiqui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
